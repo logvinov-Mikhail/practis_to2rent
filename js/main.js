becc87762c5085a68e574f1c85128d7ee4d543d7
Logvinov Mikhail <mlogvinov2016@gmail.com>
2021-02-17 00:12:36 +0300

    adфptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function(){
     $('.slider').slick({
         nextArrow:'<button class="slick-arrow slick-next"> <img src="images/next.svg" alt="next arrow"></button>',
         prevArrow:'<button class="slick-arrow slick-prev"> <img src="images/prev.svg" alt="prev arrow"></button>',
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
